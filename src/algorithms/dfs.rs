6a660678baf443c71bee1def434ef5b0481f8f90
David Martschenko <david.martschenko@web.de>
2022-10-25 12:47:36 +0200

    miri should fail

--- src/algorithms/dfs.rs
+++ src/algorithms/dfs.rs
@@ -1,4 +1,10 @@
+use std::mem::MaybeUninit;
+
 pub fn sub(left: usize, right: usize) -> usize {
+    unsafe {
+        let arr: [MaybeUninit<u32>; 3] = MaybeUninit::uninit().assume_init();
+        println!("{}", arr[2].assume_init());
+    }
     left - right
 }
 
