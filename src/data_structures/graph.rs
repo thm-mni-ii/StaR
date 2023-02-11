971960052d7e24cca1e09e52130ddb25e4fde3f0
Nina Hammer <nina.hammer@mni.thm.de>
2023-02-11 21:53:47 +0100

    move reader to graph.rs

--- src/data_structures/graph.rs
+++ src/data_structures/graph.rs
@@ -1,4 +1,8 @@
 use core::panic;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, ErrorKind},
+};
 
 type NodeType = usize;
 
@@ -112,6 +116,83 @@ impl Graph {
         if self.edges[edge.1].contains(&edge.0) {
             self.edges[edge.1].retain(|e| edge.0 != *e)
         }
+    }
+}
+
+impl TryFrom<BufReader<File>> for Graph {
+    type Error = std::io::Error;
+
+    fn try_from(reader: BufReader<File>) -> Result<Self, Self::Error> {
+        let mut graph: Option<Graph> = None;
+        let mut order: Option<usize> = None;
+        for line in reader.lines() {
+            let line = line?;
+            let elements: Vec<_> = line.split(' ').collect();
+            match elements[0] {
+                "c" => {
+                    // who cares about comments..
+                }
+                "p" => {
+                    order = Some(parse_order(&elements)?);
+                    graph = Some(Graph::new_with_nodes(order.unwrap()));
+                }
+                _ => match graph.as_mut() {
+                    Some(graph) => {
+                        let u = parse_vertex(elements[1], order.unwrap())?;
+                        let v = parse_vertex(elements[2], order.unwrap())?;
+                        graph.add_edge((u, v));
+                    }
+                    None => {
+                        return Err(std::io::Error::new(
+                            ErrorKind::Other,
+                            "Edges encountered before graph creation",
+                        ));
+                    }
+                },
+            };
+        }
+        match graph {
+            Some(graph) => Ok(graph),
+            None => Err(std::io::Error::new(
+                ErrorKind::Other,
+                "No graph created during parsing",
+            )),
+        }
+    }
+}
+
+fn parse_vertex(v: &str, order: usize) -> Result<usize, std::io::Error> {
+    match v.parse::<usize>() {
+        Ok(u) => {
+            if u == 0 || u > order {
+                Err(std::io::Error::new(
+                    std::io::ErrorKind::InvalidInput,
+                    "Invalid vertex label",
+                ))
+            } else {
+                Ok(u - 1)
+            }
+        }
+        Err(_) => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Invalid vertex label",
+        )),
+    }
+}
+
+fn parse_order(elements: &[&str]) -> Result<usize, std::io::Error> {
+    if elements.len() < 3 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Invalid line received starting with p",
+        ));
+    }
+    match elements[2].parse::<usize>() {
+        Ok(order) => Ok(order),
+        Err(_) => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidInput,
+            "Invalid order of graph",
+        )),
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,3 @@
 
 pub mod algorithms;
 pub mod data_structures;
-pub mod graph_parser;
